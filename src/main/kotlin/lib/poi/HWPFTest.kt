feee7a8c2c50a7bd4b8e624f291159543794483d
wenmin92 <wenmin92@gmail.com>
2019-12-03 18:39:51 +0800

    [+] test POI

--- src/main/kotlin/lib/poi/HWPFTest.kt
+++ src/main/kotlin/lib/poi/HWPFTest.kt
@@ -1,8 +1,14 @@
 package lib.poi
 
+import org.apache.poi.hwpf.HWPFDocument
+import org.apache.poi.poifs.filesystem.POIFSFileSystem
 import java.io.File
 
+class Test
+
 fun main() {
-    println(File("test.txt").absolutePath)
-    println(System.getProperty("user.dir"))
+    val template = File(ClassLoader.getSystemResource("empty.doc").path)
+    val doc = HWPFDocument(template.inputStream())
+    val overallRange = doc.overallRange
+    doc.write(File(File(ClassLoader.getSystemResource("empty.doc").path).parentFile, "tmp.doc"))
 }
