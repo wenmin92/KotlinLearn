58f4044bc32f2345dfcabd33ac8f1a51839252a0
wenmin92 <wenmin92@gmail.com>
2019-12-04 14:20:34 +0800

    [*] test POI

--- src/main/kotlin/lib/poi/HWPFTest.kt
+++ src/main/kotlin/lib/poi/HWPFTest.kt
@@ -4,11 +4,18 @@ import org.apache.poi.hwpf.HWPFDocument
 import org.apache.poi.poifs.filesystem.POIFSFileSystem
 import java.io.File
 
-class Test
-
 fun main() {
     val template = File(ClassLoader.getSystemResource("empty.doc").path)
     val doc = HWPFDocument(template.inputStream())
-    val overallRange = doc.overallRange
+    val range = doc.range
+    // 无法插入段落，写入功能就很鸡肋了。
+    range.insertAfter("我爱Java").run {
+        isBold = false
+        color = 5
+        underlineCode = 3
+    }
+    range.insertAfter("\r")
+    val section = range.getSection(0)
+    val paragraph = range.getParagraph(0)
     doc.write(File(File(ClassLoader.getSystemResource("empty.doc").path).parentFile, "tmp.doc"))
 }
